bb41692cf7588a055831df3e6ad13b06af4b802a
juanjoelmono31 <juanjoelmono31@gmail.com>
2023-04-25 13:35:03 -0500

    Se agrego el campo fonod_cesantias

--- src/controllers/affiliation.controller.ts
+++ src/controllers/affiliation.controller.ts
@@ -21,7 +21,7 @@ export async function creatAffiliation(req: Request , res: Response){
     const {cedula, nombre,apellido, genero, fecha_nacimiento,
         direccion, correo, celular, telefono, nombre_emergencia,
         celular_emergencia,  fecha_ingreso, examen_ingreso, salario,
-        cargo, curso_alturas, rut, eps, arl, fondo_pensiones, fondo_cesantias, caja_compensacion,
+        cargo, curso_alturas, rut, eps, arl, fondo_pensiones, fonod_cesantias, caja_compensacion,
         estado, numero_cuenta, entidad_bancaria, aux_admin_revision, whatsapp,telegram, asistencia, proyectos, nameProyecto, dias_laborados, fecha_retiro} = req.body
         
         
@@ -30,7 +30,7 @@ export async function creatAffiliation(req: Request , res: Response){
             cedula: cedula, nombre: nombre, apellido: apellido, genero: genero, fecha_nacimiento: fecha_nacimiento,
             direccion: direccion, correo: correo, celular:celular, telefono: telefono, nombre_emergencia: nombre_emergencia,
             celular_emergencia: celular_emergencia, fecha_ingreso: fecha_ingreso, examen_ingreso: examen_ingreso, salario: salario,
-            cargo: cargo, curso_alturas: curso_alturas, rut:rut, eps:eps, arl: arl, fondo_pensiones: fondo_pensiones, fondo_cesantias: fondo_cesantias, caja_compensacion: caja_compensacion,
+            cargo: cargo, curso_alturas: curso_alturas, rut:rut, eps:eps, arl: arl, fondo_pensiones: fondo_pensiones, fonod_cesantias: fonod_cesantias, caja_compensacion: caja_compensacion,
             estado: estado, numero_cuenta: numero_cuenta, entidad_bancaria: entidad_bancaria, aux_admin_revision: aux_admin_revision, whatsapp: whatsapp,
             telegram: telegram, archivos: req.file, asistencia: asistencia, proyectos: proyectos, nameProyecto: nameProyecto, dias_laborados: dias_laborados, fecha_retiro: fecha_retiro   
             
@@ -76,11 +76,11 @@ export async function findOneAffiliation(req: Request, res: Response): Promise<R
 export async function updateAffiliation(req: Request, res: Response): Promise<Response> {
     const { id } = req.params
     const { correo, rut, curso_alturas, examen_ingreso, eps, arl,fondo_pensiones,
-        fondo_cesantias, caja_compensacion, entidad_bancaria, numero_cuenta, proyectos } = req.body
+         caja_compensacion, entidad_bancaria, numero_cuenta, proyectos, fonod_cesantias } = req.body
 
     const updatedAffiliation = await Affiliation.findByIdAndUpdate(id, {
         correo, rut, curso_alturas, examen_ingreso, eps, arl,fondo_pensiones,
-        fondo_cesantias, caja_compensacion, entidad_bancaria, numero_cuenta, proyectos
+         caja_compensacion, entidad_bancaria, numero_cuenta, proyectos, fonod_cesantias
     })
 
     return res.json({
@@ -117,6 +117,7 @@ export async function updateAsistencia(req: Request, res: Response): Promise<Res
     })
 }
 
+//Actulizar fecha retiro
 export async function updateFechaRetiro(req: Request, res: Response): Promise<Response> {
     const { id } = req.params
     const { fecha_retiro  } = req.body
@@ -145,12 +146,14 @@ export async function updateDiasLaborados(req: Request, res: Response) {
     })
 }
 
+
+
 //Actualizar campos Bancarios y datos de afiliacion
 export async function updateDatos(req: Request, res: Response) {
     const { id } = req.params
-    const { eps,  arl, fondo_pensiones, caja_compensacion, entidad_bancaria, numero_cuenta, rut, curso_alturas, examen_ingreso} = req.body
+    const { eps,  arl, fondo_pensiones, caja_compensacion, entidad_bancaria, numero_cuenta, rut, curso_alturas, examen_ingreso, fonod_cesantias} = req.body
     const updatedDatos = await Affiliation.findByIdAndUpdate(id, {
-        eps, arl, fondo_pensiones, caja_compensacion, entidad_bancaria, numero_cuenta, rut, curso_alturas, examen_ingreso
+        eps, arl, fondo_pensiones, caja_compensacion, entidad_bancaria, numero_cuenta, rut, curso_alturas, examen_ingreso, fonod_cesantias
     })
 
     return res.json({
